b00e6c15407d451256fb10784326d572d08ca454
devrathrana <devrath.rana@bold.com>
2024-11-25 01:47:54 +0530

    JS

---
+++ 2_datatypes.js
@@ -0,0 +1,48 @@
+/*
+Create a variable learningPlatform and set it to "codedamn"
+
+Create a variable myAge. This variable should be a number
+
+Create a variable called amIAbove18. This variable should have either true or false as the value
+
+*/
+// write your code below
+
+const name = "DEVRATH";
+const standard = 4;
+const isEligible = false;
+let undefinedSpecialVar;
+const nullSpecialVar = null;
+const bigIntegerVar = 11111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111n;
+
+const learningPlatform  = "codedamn";
+const myAge  = 23;
+const amIAbove18  = true;
+
+console.log(name);
+console.log(standard);
+console.log(isEligible);
+console.log(undefinedSpecialVar);
+console.log(nullSpecialVar);
+console.log(bigIntegerVar);
+
+console.log(learningPlatform);
+console.log(myAge);
+console.log(amIAbove18);
+
+/*
+
+Q) What is the output of the below JavaScript Expression
+
+typeof null
+
+A) object
+
+Explaination - https://s3.us-east-1.amazonaws.com/creator-assets.codedamn.com/official-61897bfe60f1140008feb00d/MARKDOWN/2023-06-24/d200367a9d7e7ea145579fdd65a58bf008cb5565
+
+ The `type` of `null` in JavaScript is `objectt` and not `null`. You might be wondering why this is the case. This is a special Exception in JavaScript. You can find out more about this from [MDN's Documentation](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/typeof#typeof_null)
+
+ Q) What will be the output of "typeof 1337"?
+ A) number
+
+*/
---
+++ 3_mathematical_operations.js
@@ -0,0 +1,19 @@
+/*
+Power of 5
+In this lab, you have to work with exponential mathematical operator and set a number to power 5.
+*/
+
+const two = 2;
+// write the correct expression below
+const twoToThePower5 = two ** 5;
+
+const someNumber = 329826;
+// write the correct expression here
+const someNumberToThePowerOf5 = someNumber ** 5;
+console.log("twoToThePower5>>", twoToThePower5);
+console.log("someNumberToThePowerOf5>>", someNumberToThePowerOf5);
+
+/**const will give reassignment error for myAge */
+let myAge = 5;
+myAge++;
+console.log("myAge>>>", myAge);
