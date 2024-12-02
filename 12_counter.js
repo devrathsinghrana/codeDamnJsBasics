3529b8ad0ab7f4a89ae442fd046baf1b0121471a
devrathrana <devrath.rana@bold.com>
2024-12-03 00:30:29 +0530

    js

--- 12_counter.js
+++ 12_counter.js
@@ -1,12 +1,23 @@
 const incrementBtn = document.querySelector(".increment");
 const decrementBtn = document.querySelector(".decrement");
 const counter = document.querySelector("#counter");
-
+const body = document.querySelector("body");
+const ul = document.createElement("ul");
+ul.innerHTML = "<h2>Counter sentence list</h2>";
+body.appendChild(ul);
 let counterValue = 0;
 
 function incrementValue() {
   counterValue++;
   counter.innerText = counterValue;
+  const li = document.createElement("li");
+  const b = document.createElement("b");
+  const sentence = document.createTextNode(`Sentence `);
+  b.appendChild(sentence);
+  const counterText = document.createTextNode(counterValue);
+  li.appendChild(b);
+  li.appendChild(counterText);
+  ul.appendChild(li);
 }
 
 function decrementValue() {
@@ -15,5 +26,5 @@ function decrementValue() {
   counter.innerText = counterValue;
 }
 
-incrementBtn.addEventListener("click", incrementValue)
-decrementBtn.addEventListener("click", decrementValue)
+incrementBtn.addEventListener("click", incrementValue);
+decrementBtn.addEventListener("click", decrementValue);
