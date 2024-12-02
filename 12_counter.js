120133bc48cbd61e1b391640984f59e8503c5f01
devrathrana <devrath.rana@bold.com>
2024-12-03 00:56:00 +0530

    js

--- 12_counter.js
+++ 12_counter.js
@@ -11,6 +11,7 @@ function incrementValue() {
   counterValue++;
   counter.innerText = counterValue;
   const li = document.createElement("li");
+  li.setAttribute("data-counter", counterValue);
   const b = document.createElement("b");
   const sentence = document.createTextNode(`Sentence `);
   b.appendChild(sentence);
@@ -21,6 +22,14 @@ function incrementValue() {
 }
 
 function decrementValue() {
+  const currentCounterLi = document.querySelector(
+    `[data-counter="${counterValue}"]`
+  );
+  const currentCounterAttrValue = parseInt(
+    currentCounterLi.getAttribute("data-counter"),
+    10
+  );
+  if (currentCounterAttrValue % 2 === 0) currentCounterLi.remove();
   if (counterValue === 0) return;
   counterValue--;
   counter.innerText = counterValue;
